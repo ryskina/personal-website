f7e6a9511db7dc6da67a3f487fbacdae2f4245b5
Maria Ryskina <m.ryskina@gmail.com>
2021-11-26 13:35:07 -0500

    Reverted to default theme choice

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -39,11 +39,11 @@ let transTheme = () => {
 
 let initTheme = (theme) => {
   if (theme == null) {
-      theme = 'light';
-//    const userPref = window.matchMedia;
-//    if (userPref && userPref('(prefers-color-scheme: dark)').matches) {
-//        theme = 'dark';
-//    }
+    // theme = 'light';
+    const userPref = window.matchMedia;
+    if (userPref && userPref('(prefers-color-scheme: dark)').matches) {
+        theme = 'dark';
+    }
   }
   setTheme(theme);
 }
